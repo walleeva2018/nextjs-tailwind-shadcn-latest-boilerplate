68d7558d7d27fcdc2b94273993e3732c31ceecf1
zubairrafi <walleeva2018@gmail.com>
2025-05-14 22:57:20 +0600

    Init project
    Signed-off-by: zubairrafi <walleeva2018@gmail.com>

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,7 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
-};
-
-export default config;
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  plugins: {
+    '@tailwindcss/postcss': {},
+  },
+}
